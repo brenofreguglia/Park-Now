b111d5458fe3a9d600244a5aceaee6d06a254e6a
Breno Freguglia <freguglia.breno@gmail.com>
2024-04-18 12:52:30 -0300

    a
    a

--- ParkNow/src/Componentes/Textos.js
+++ ParkNow/src/Componentes/Textos.js
@@ -1,28 +1,32 @@
 import React from "react";
-import { Text, StyleSheet, Dimensions, TextInput} from "react-native";
+import { Text, StyleSheet, Dimensions, TextInput } from "react-native";
 
-const Lg = Dimensions.get('screen').width
+const Lg = Dimensions.get("screen").width;
 
-const Texto = ({msg, tamanho, color}) => {
-    return (
-       <Text style = {[styles.texto,{fontSize: tamanho, color: color}]}>
-                {msg}
-         </Text>
-    )
-}
-const TextoInput = ({msg, tamanho, color, width, borda}) => {
-    return (
-       <TextInput placeholder="Digite Aqui" style ={{backgroundColor: color, width: width , borderRadius: borda}}>{msg}</TextInput>
-    )
-}
-
+const Texto = ({ msg, tamanho, color }) => {
+  return (
+    <Text style={[styles.texto, { fontSize: tamanho, color: color }]}>
+      {msg}
+    </Text>
+  );
+};
+const TextoInput = ({ msg, tamanho, color, width, borda }) => {
+  return (
+    <TextInput
+      placeholder="Digite Aqui"
+      style={{ backgroundColor: color, width: width, borderRadius: borda }}
+    >
+      {msg}
+    </TextInput>
+  );
+};
 
 const styles = StyleSheet.create({
-    texto:{
-        justifyContent: "center",
-        textAlign: "center",
-        fontWeight: "bold",
-        width: Lg/2,
-    },
-})
-    export {Texto,TextoInput}
+  texto: {
+    justifyContent: "center",
+    textAlign: "center",
+    fontWeight: "bold",
+    width: Lg / 2,
+  },
+});
+export { Texto, TextoInput };
--- ParkNow/src/Screens/Login.js
+++ ParkNow/src/Screens/Login.js
@@ -3,70 +3,57 @@ import { Button } from "../Componentes/Buttons";
 import { Texto, TextoInput } from "../Componentes/Textos";
 import { useNavigation } from "@react-navigation/native";
 
-
-
 export default function Login({}) {
+  const navigation = useNavigation();
 
-    const navigation = useNavigation();
-  
-    const cadastrar_se = (indice) => {
-    
-
-
-        const produtoSelecionado = produtos[indice]
-        navigation.navigate('Cadastrar_se',{item:produtoSelecionado})
-    
-    }
-
+  const cadastrar_se = (indice) => {
+    const produtoSelecionado = produtos[indice];
+    navigation.navigate("Cadastrar_se", { item: produtoSelecionado });
+  };
 
   return (
     <ScrollView>
-        
-        <View style={styles.container}>
+      <View style={styles.container}>
+        <Image
+          source={require("../../assets/Imgs/imagem.png")}
+          style={{ height: 250, margin: 30 }}
+        />
 
-        <Image source={require("../../assets/Imgs/imagem.png")} style={{height:250, margin: 30}}/>
-        
-            <View style={styles.container_2}>
-                
+        <View style={styles.container_2}>
+          <TextoInput color={"#D2F0EE"} width={330} borda={30} />
 
-
-              
-                    <TextoInput style={styles.input} />
-                        <TextoInput style={styles.input} color={"#D2F0EE"} width ={330} borda={30}/>
-                        <Button texto={"Login"}/>
-                        <Texto msg={"Ainda não tem uma conta ?"} onPress={() => cadastrar_se(index)} style={{ color: "red" }} ></Texto>
-            
-
-
-
-            </View>
-
-        </View>   
-
+          <TextoInput color={"#D2F0EE"} width={330} borda={30} />
+          <Button texto={"Login"} />
+          <Texto
+            msg={"Ainda não tem uma conta ?"}
+            onPress={() => cadastrar_se(index)}
+            style={{ color: "red" }}
+          ></Texto>
+        </View>
+      </View>
     </ScrollView>
-  ) 
-  }
-  
-
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: "#D2F0EE",
-    },
-    container_2: {
-        flex: 1,
-        backgroundColor: "#73D2C0",
-        marginTop: "10%",
-        width: "100%",
-        flexDirection: "column",
-        justifyContent: 'space-evenly',
-        flexDirection: "column",
-        alignItems: "center",
-
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#D2F0EE",
+    height: "50%",
+  },
+  container_2: {
+    flex: 1,
+    backgroundColor: "#73D2C0",
+    // marginTop: "10%",
+    width: "100%",
+    flexDirection: "column",
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center",
     
-  });
+    height: 400,
+  },
+});
