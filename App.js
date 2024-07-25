9b7aef295175ffb63a19d6b08063795c6a93272a
Breno Freguglia <90163639+brenofreguglia@users.noreply.github.com>
2024-07-25 15:33:03 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Image, Text, StyleSheet, View } from "react-native"; 
+import { ButtonTitle } from './src/Componentes/Buttons';
+import 'react-native-gesture-handler';
+
+
+import Menu from "./src/Screens/Menu";
+import FaleConosco from "./src/Screens/FaleConosco";
+import Login from "./src/Screens/Login";
+import Cadastro from "./src/Screens/Cadastro";
+import Estacionamento from './src/Screens/Estacionamento';
+import Perfil from './src/Screens/Perfil';
+
+function LogoTitle() {
+
+  const navigation = useNavigation();
+
+  const Perfil = () => {
+    navigation.navigate("Perfil" ,{screen: "Perfil"});
+  };
+
+
+  return (
+    <View style={styles.header}>
+      <View style={{ flexDirection: 'row', alignItems: 'center'}}>
+        <Text style={{ color: "black", marginRight: 40, textAlign: "center", left: 100, fontSize: 20, fontWeight: "bold"}}>HOME</Text>
+        <ButtonTitle acao={Perfil} />
+      </View>
+    </View>
+  );
+}
+
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator screenOptions={{ headerShown: false }}>
+
+        <Drawer.Screen name="Menu" component={Menu} options={{ 
+          headerStyle: { backgroundColor: "#D2F0EE" },
+          headerTitle: (props) => <LogoTitle {...props} />,
+          headerShown: true}} />
+
+        <Drawer.Screen name="Perfil" component={Perfil} />
+
+        <Drawer.Screen name='Estacionamento' component={Estacionamento} />  
+
+        <Drawer.Screen name="Login" component={Login} options={{ 
+          headerShown: false }} />
+
+
+
+
+        <Drawer.Screen name="Cadastro" component={Cadastro} options={{ headerShown: false }} />
+        <Drawer.Screen name="FaleConosco" component={FaleConosco} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
