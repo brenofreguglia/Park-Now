56744e29a8f7c8280e417e17fdc6177a9de1fe81
Breno Freguglia <freguglia.breno@gmail.com>
2024-07-25 15:40:38 -0300

    Atualização nova

---
+++ back-end/app.js
@@ -0,0 +1,150 @@
+const express = require("express")
+const cors = require("cors")
+const mysql = require("mysql2/promise")
+const bodyparser = require("body-parser")
+
+const app = express()
+const porta = 3000
+app.use(cors())
+app.use(bodyparser.json())
+
+const pool = mysql.createPool({
+    host: `localhost`,
+    user: `root`,
+    password: ``,
+    database: `parknow`,
+    waitForConnections: true,
+    connectionLimit: 3,
+    queueLimit: 0
+})
+
+
+// Rota pra consulta
+app.get("api/parknow/consulta", async (req, res) => {
+    try {
+        const conexao = await pool.getConnection();
+        const sql = `SELECT c.nome, c.id, c.ibge, i.nome AS nome_cidade, c.cep, i.uf
+        FROM clientes c, ibge i
+        WHERE i.codigo = c.ibge`;
+        const [linha] = await conexao.execute(sql);
+        conexao.release();
+        res.json(linha);
+    } catch (error) {
+        console.log(`O erro que ocorreu foi: ${error}`);
+        res.status(500).json({ error: "Deu algum erro na busca" });
+    }
+});
+
+
+//  ROTA PRA CADASTRAR O CLIENTE
+app.post("/api/parknow/cadastro", async (req, res) => {
+    try {
+        const { nome, sobrenome, usuario, cpf, endereco, cep, telefone, email, senha } = req.body
+        const conexao = await pool.getConnection()
+        let sql = `INSERT INTO cadastro (nome, sobrenome, username, cpf, endereco, cep, telefone,
+        email, senha) VALUE ("${nome}", "${sobrenome}","${usuario}","${cpf}","${endereco}","${cep}","${telefone}",
+        "${email}","${senha}")`
+        console.log(sql)
+        const [linhas] = await conexao.execute(sql)
+        conexao.release()
+        res.json({ msg: "Registro gravado!" })
+
+    } catch (error) {
+        console.log(`O Erro que ocorreu foi :${error}`)
+        res.status(500).json({ error: "Deu algum erro no cadastro" })
+    }
+});
+
+
+//ROTA LOGIN DO CLIENTE
+app.post("/api/parknow/login", async (req, res) => {
+    try {
+        const { id, email, senha } = req.body
+        const conexao = await pool.getConnection()
+        let sql = `INSERT INTO login (id, email, senha) VALUE ("${id}","${email}","${senha}")`
+        console.log(sql)
+        const [linhas] = await conexao.execute(sql)
+        conexao.release()
+        res.json({ msg: "Registro gravado!" })
+
+    } catch (error) {
+        console.log(`O Erro que ocorreu foi :${error}`)
+        res.status(500).json({ error: "Deu algum erro no cadastro" })
+    }
+});
+
+
+// ROTA PRA CADASTRAR O LOCAL 
+app.post("/api/parknow/cadastro", async (req, res) => {
+
+    try {
+        const { id_lugar, nome, cidade, endereco, cep, vagas, func_horarios } = req.body
+        const conexao = await pool.getConnection()
+        let sql = `INSERT INTO local (id_lugar, nome, cidade, endereco, cep, vagas, func_horarios) VALUE 
+        ("${id_lugar}", "${nome}", "${cidade}", "${endereco}", "${cep}", "${vagas}", "${func_horarios}")`
+        console.log(sql)
+        const [linhas] = await conexao.execute(sql)
+        conexao.release()
+        res.json({ msg: "Registro gravado!" })
+    } catch (error) {
+        console.log(`O Erro que ocorreu foi :${error}`)
+        res.status(500).json({ error: "Deu algum erro no cadastro" })
+    }
+
+});
+
+
+// ROTA DO CLIENTE
+app.get("/api/parknow/cliente", async (req, res) => {
+
+    try {
+        const { id_cliente, nome, tipo_veiculo, endereco, cep, placa_automovel, } = req.body
+        const conexao = await pool.getConnection()
+        let sql = `SELECT *`
+        console.log(sql)
+        const [linhas] = await conexao.execute(sql)
+        conexao.release()
+        res.json({ msg: "Registro gravado!" })
+    } catch (error) {
+        console.log(`O Erro que ocorreu foi :${error}`)
+        res.status(500).json({ error: "Deu algum erro no cadastro" })
+    }
+
+});
+
+// ROTA PRA DELETAR CONTA DO CLIENTE
+app.delete("/api/parknow/cadastro/:id", async (req, res) => {
+    try {
+        const id_passado = req.params.id
+        const conexao = await pool.getConnection()
+        const sql = `DELETE FROM usuarios WHERE id = "${id_passado}"`
+        console.log(sql)
+        const [linhas] = await conexao.execute(sql)
+        conexao.release()
+        res.json({ msg: "Registro excluído!" })
+
+    } catch (error) {
+        console.log(`O Erro que ocorreu foi :${error}`)
+        res.status(500).json({ error: "Deu algum erro na exclusão" })
+    }
+})
+
+// ROTA PRA EDITAR CONTA DO CLIENTE
+app.put("/api/parknow/cadastro/", async (req, res) => {
+    try {
+        const { id, nome, email,sobrenome,username,cpf,endereco,cep,telefone,senha } = req.body
+        const conexao = await pool.getConnection()
+        const sql = `UPDATE usuarios SET nome = "${nome}", sobrenome = "${sobrenome}", username = "${username}", cpf = "${cpf}",
+        endereco = "${endereco}", cep = "${cep}", telefone = "${telefone}", email = "${email}", senha = "${senha}" WHERE id = ${id}`
+        console.log(sql)
+        const [linhas] = await conexao.execute(sql)
+        conexao.release()
+        res.json({ msg: "Registro gravado!" })
+
+    } catch (error) {
+        console.log(`O Erro que ocorreu foi :${error}`)
+        res.status(500).json({ error: "Deu algum erro na edição" })
+    }
+})
+
+app.listen(porta, () => console.log(`ver rodando em porta ${porta}`))
---
+++ src/Screens/Login.js
@@ -0,0 +1,77 @@
+import { StyleSheet, View, Image, ScrollView, Dimensions } from "react-native";
+import { Button } from "../Componentes/Buttons";
+import { Texto, TextoInput } from "../Componentes/Textos";
+import { useNavigation } from "@react-navigation/native";
+import Cadastro from "./Cadastro";
+
+const al = Dimensions.get("screen").height;
+
+export default function Login({}) {
+  const navigation = useNavigation();
+
+  const Cadastro = () => {
+    navigation.navigate("Cadastro" ,{screen: "Cadastro"});
+  };
+
+  return (
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <View style={styles.container}>
+        <Image
+          source={require("../../assets/Imgs/imagem.png")}
+          style={styles.image}
+        />
+        <View style={styles.container_2}>
+          <TextoInput
+            color={"#D2F0EE"}
+            width={330}
+            borda={30}
+            margin={20}
+            height={60}
+            lugar={"center"}
+            tamanho={20}
+            holder={"E-mail"}
+          />
+          <TextoInput
+            color={"#D2F0EE"}
+            width={330}
+            borda={30}
+            height={60}
+            lugar={"center"}
+            tamanho={20}
+            holder={"Senha"}
+          />
+            <Button 
+            texto={"Login"} 
+            texcolor={"white"} />
+            <Texto
+            acao={Cadastro}
+            msg={"Ainda não possui uma conta? Cadastre-se"}
+            tamanho={15}
+            margin={20}
+            cor={"#D9D9D9"}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#D2F0EE",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container_2: {
+    backgroundColor: "#73D2C0",
+    width: "100%",
+    alignItems: "center",
+    paddingVertical: 60,
+  },
+  image: {
+    height: 250,
+    margin: 70,
+    resizeMode: "contain",
+  },
+});
